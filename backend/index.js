ce4a6d71fe7516692d6d8c5018eb03daed7abbd7
Prashant2002pd <92350732+Prashant2002pd@users.noreply.github.com>
2024-09-05 14:10:48 +0530

    added socket.io

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const routes = require('./routers/routes');
-const connect=require("./connection/db")
-const cors = require('cors')
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
+const server = createServer(app);
+const io = new Server(server);
+const routes = require("./routers/routes");
+const connect = require("./connection/db");
+const cors = require("cors");
 
-app.use(cors())
-connect
+app.use(cors());
+connect;
 // Use the auth routes
-app.use(express.json())
-app.use('/api', routes);
+app.use(express.json());
+app.use("/api", routes);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
